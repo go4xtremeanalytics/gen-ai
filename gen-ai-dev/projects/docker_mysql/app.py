1e05cfd7ecfa440b09fda2360e9d5d7257cc39e9
go4xtremeanalytics <go4xtremeanalytics@gmail.com>
2024-08-13 00:25:10 -0500

    edit with secrets

--- gen-ai-dev/projects/docker_mysql/app.py
+++ gen-ai-dev/projects/docker_mysql/app.py
@@ -5,32 +5,42 @@ import pandas as pd
 
 # Database connection
 # This is for testing
-@st.cache_resource
-def init_connection():
-    return mysql.connector.connect(
-        host=os.getenv("MYSQL_HOST", 'localhost'), # Changed from "host.docker.internal" 
-        user=os.getenv("MYSQL_USER", "root"),
-        password=os.getenv("MYSQL_PASSWORD", "maniKANDAN-661"),
-        database=os.getenv("MYSQL_DATABASE", "classicmodels")
-    )
+# @st.cache_resource
+# def init_connection():
+#     return mysql.connector.connect(
+#         host=os.getenv("MYSQL_HOST", 'localhost'), # Changed from "host.docker.internal" 
+#         user=os.getenv("MYSQL_USER", "root"),
+#         password=os.getenv("MYSQL_PASSWORD", "maniKANDAN-661"),
+#         database=os.getenv("MYSQL_DATABASE", "classicmodels")
+#     )
 
-conn = init_connection()
 
-# Perform query
-@st.cache_data
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT customerNumber, customerName, phone FROM customers LIMIT 10;', ttl=600)  
+
+
+
+# conn = init_connection()
+
+# # Perform query
+# @st.cache_data
+# def run_query(query):
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         return cur.fetchall()
 
 # Streamlit app
 st.title('My Streamlit App with MySQL Data')
 
 # Example query
-query = "SELECT customerNumber, customerName, phone FROM customers LIMIT 10"
-rows = run_query(query)
+# query = "SELECT customerNumber, customerName, phone FROM customers LIMIT 10"
+# rows = run_query(query)
 
 # Display data
 st.write("Data from MySQL:")
-df = pd.DataFrame(rows, columns=["customerNumber", "customerName", "phone"])  # Replace with your actual column names
+# df = pd.DataFrame(rows, columns=["customerNumber", "customerName", "phone"])  # Replace with your actual column names
 st.dataframe(df)
